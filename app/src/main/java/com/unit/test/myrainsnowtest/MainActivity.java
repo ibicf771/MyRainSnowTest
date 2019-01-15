26a361c88643abb55bc166223b8abcc06b1c1f16
yangsimin <181326622@qq.com>
2019-01-15 15:27:25 +0800

    添加闪电效果

--- app/src/main/java/com/unit/test/myrainsnowtest/MainActivity.java
+++ app/src/main/java/com/unit/test/myrainsnowtest/MainActivity.java
@@ -1,19 +1,68 @@
 package com.unit.test.myrainsnowtest;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-import com.unit.test.myrainsnowtest.snowfall.SnowfallView;
-import com.unit.test.myrainsnowtest.snowfall.TestClass;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private View lightningLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        SnowfallView snowfallView = new SnowfallView(MainActivity.this,null);
-        TestClass testClass = new TestClass();
+        lightningLayout = findViewById(R.id.lightning_layout);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                lightningLayout.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        lightningLayout.setVisibility(View.GONE);
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                lightningLayout.setVisibility(View.VISIBLE);
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        lightningLayout.setVisibility(View.GONE);
+                                    }
+                                }, 100);
+                            }
+                        }, 100);
+                    }
+                }, 100);
+            }
+        }, 1000);
+
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                lightningLayout.setVisibility(View.VISIBLE);
+//                handler.postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        lightningLayout.setVisibility(View.GONE);
+//                    }
+//                },200);
+//            }
+//        }, 1200);
+//
+//        Timer timer = new Timer();
+//        TimerTask timerTask = new TimerTask() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        };
+//        timer.schedule(timerTask, 1000, 10);
     }
 }
